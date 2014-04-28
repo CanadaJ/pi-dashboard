7e24d0696ddcdaeee3edb4281dbb3142058aa173
Justin C <justin.d.canada@gmail.com>
2014-04-27 21:54:05 -0700

    removed gridster plugin, and went with simpler layout. added temp api for rpi

--- public/js/site.js
+++ public/js/site.js
@@ -1,27 +1,15 @@
 $(function() {
-	var diskWidget = '<li class=diskWidget><div class="widget-title"><span class="title">Disk Usage</span><span class="glyphicon glyphicon-remove"></span></div><div class="widget-content"><pre class="prettify lang-sh"></pre></div></li>';
-
-	var gridster = $('.gridster ul').gridster({
-		widget_margins: [10,10],
-		widget_base_dimensions: [140, 140],
-		resize: {
-			enabled: true
+	$.ajax({
+		url: '/api/disk',
+		success: function(data) {
+			$('.disk-pre').text(data);
 		}
-	}).data('gridster');
+	});
 
-	gridster.add_widget(diskWidget, 2, 1);
-
-	setInterval(function() {
-		$.ajax({
-			url: '/api/disk',
-			success: function(result, status, xhr) {
-				$('.diskWidget .widget-content .prettify').text(result);
-			}
-		})
-	}, 3000);
-
-	$('.glyphicon-remove').click(function() {
-		$(this).parent().parent().addClass("removeMe");
-		gridster.remove_widget(".removeMe");
+	$.ajax({
+		url: '/api/temp',
+		success: function(data) {
+			$('.temp-pre').text(data);
+		}
 	})
 });
--- server.js
+++ server.js
@@ -9,10 +9,30 @@ app.use(express.compress());
 app.use(express.static(__dirname+ '/public'));
 
 app.get('/api/disk', function(req, res) {
-	exec("df -h", function(error, stdout, stderr) {
+	exec('df -h', function(error, stdout, stderr) {
 		res.send(stdout);
-	})
-})
+	});
+});
+
+app.get('/api/temp', function(req, res) {
+	var cpu_temp = '';
+	var gpu_temp = '';
+	exec('cat /sys/class/thermal/thermal_zone0/temp', function(error, stdout, stderr) {
+		if(error !== null) {
+			cpu_temp = stderr;
+		} else {
+			cpu_temp = 'CPU Temp: ' + (stdout/100 % stdout/1000)
+		}
+		exec('/opt/vc/bin/vcgencmd measure_temp', function(error, stdout, stderr) {
+			if(error !== null) {
+				gpu_temp = stderr;
+			} else {
+				gpu_temp = 'GPU Temp: ' + stdout;
+			}
+			res.send(cpu_temp + '\n' + gpu_temp);
+		});
+	});	
+});
 
 app.listen(process.env.PORT || 3000);
 console.log('listening on 3000');
